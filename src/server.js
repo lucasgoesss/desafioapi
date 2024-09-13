42a1effa8015b9da6d92f15a164f5c0619b102a2
lucasgoesss <114628371+lucasgoesss@users.noreply.github.com>
2024-09-13 20:43:24 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,56 +18,6 @@ const PORT = 3334;
 
 migrationsRun();
 
-// app.get("/", (request, response) => {
-//   response.status(200).send({ status: "API ok" });
-// });
-
-// ROUTE PARAMS (:)
-// QUERY PARAMS (?)
-// BODY PARAMS ({})
-
-/**
- * index - GET para listar usuários registrados
- * show - GET para exibir um registro específico
- * create - POST para criar registro
- * update - PUT para criar registro
- * delete - DELETE para criar registro
- */
-
-// app.get("/id/:id", (request, response) => {
-//   response.status(200).send({
-//     status: "API ok",
-//     id: request.params.id,
-//   });
-// });
-
-// app.get("/user/:id", (request, response) => {
-//   response.status(200).send({
-//     status: "API ok",
-//     id: request.params.id,
-//     limit: request.query.limit,
-//   });
-// });
-
-// app.use((error, _request, response, _next) => {
-//   console.error(`⛔`, error);
-
-//   if (error instanceof AppError) {
-//     return response.status(error.statusCode).json({
-//       status: "error",
-//       message: error.message,
-//       statusCode: error.statusCode,
-//     });
-//   }
-
-//   return response.status(500).json({
-//     status: "error",
-//     message: "Internal server error",
-//     messageLog: error.message,
-//     statusCode: error.statusCode,
-//   });
-// });
-
 app.listen(PORT, () =>
   console.info(`Server running in http://localhost:${PORT}`)
 );
